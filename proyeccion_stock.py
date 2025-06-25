c15a92167fc95cb34ced725ca419926f96db8e40
Sebastián Sánchez Paz <sebsforceone@gmail.com>
2025-06-25 00:15:20 -0300

    agrego funcionalidad

--- proyeccion_stock.py
+++ proyeccion_stock.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+from collections import Counter
 
 st.set_page_config(
     page_title="Mi App de Productos",
