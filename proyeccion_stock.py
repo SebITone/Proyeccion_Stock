eb91f7b02ee41ba4d976ce283b4acac4cd3c426a
Sebastián Sánchez Paz <sebsforceone@gmail.com>
2025-07-09 19:21:53 -0300

    Nuevo texto introductorio2

--- proyeccion_stock.py
+++ proyeccion_stock.py
@@ -13,7 +13,7 @@ st.set_page_config(
 st.title("📦 Proyección de Stock en base a Consumo y Vencimientos")
 st.subheader("Anticipá necesidades y evitá quiebres o vencimientos innecesarios")
 
-st.text("Esta aplicación permite estimar el consumo mensual de insumos y productos, proyectando la disponibilidad futura de stock en función de la demanda histórica y las fechas de vencimiento. Identificá con anticipación los faltantes críticos, los lotes que deben rotarse y optimizá la gestión del inventario para mejorar la toma de decisiones y reducir pérdidas.")
+st.text("Esta herramienta permite calcular la cantidad óptima de reposición por producto, teniendo en cuenta el stock disponible, los vencimientos en origen y el consumo promedio. Facilita la planificación anticipada, minimizando riesgos de quiebre de stock y optimizando las decisiones de compra en función de la proyección de cobertura.")
 
 # --- Entradas ---
 consumo_promedio = st.number_input("Consumo promedio mensual (unidades)", min_value=0.1, value=2.0)
